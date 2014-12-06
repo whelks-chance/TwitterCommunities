4b3552e6d2725f7db0e268b9b4f8b85cc70a1c5e
ian harvey <i.c.harvey@cs.cf.ac.uk>
2014-12-06 01:58:04 +0000

    redis, with optional cache avoidance

--- overlaps/AccessAPI/Compare.py
+++ overlaps/AccessAPI/Compare.py
@@ -1,3 +1,6 @@
+import json
+import redis
+
 __author__ = 'ubuntu'
 
 import twitter
@@ -9,32 +12,69 @@ class Compare():
                       consumer_secret=Consumer_Secret_API_Secret,
                       access_token_key=Access_Token,
                       access_token_secret=Access_Token_Secret)
+        self.r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 # print pprint.pformat(api.VerifyCredentials().__dict__)
 
 # print pprint.pformat(api.GetRateLimitStatus())
 
-    def get_matches(self, user1, user2):
-        u1_ids = self.api.GetFriendIDs(screen_name=user1)
+    def get_matches(self, user1, user2, allow_cached=True):
+        cached = False
 
-        u2_ids = self.api.GetFriendIDs(screen_name=user2)
+        u1_ids = self.r.get(user1 + '.following')
+        if u1_ids is None or allow_cached == False:
+            print 'redis missing ' + str(user1 + '.following')
+            u1_ids = self.api.GetFriendIDs(screen_name=user1)
+            self.r.set(user1 + '.' + 'following', json.dumps(u1_ids))
+        else:
+            cached = True
+            u1_ids = json.loads(u1_ids)
+            print 'redis had ' + str(len(u1_ids)) + ' for ' + user1
+
+        u2_ids = self.r.get(user2 + '.following')
+        if u2_ids is None or allow_cached == False:
+            print 'redis missing ' + str(user2 + '.following')
+            u2_ids = self.api.GetFriendIDs(screen_name=user2)
+            self.r.set(user2 + '.' + 'following', json.dumps(u2_ids))
+        else:
+            cached = True
+            u2_ids = json.loads(u2_ids)
+            print 'redis had ' + str(len(u2_ids)) + ' for ' + user2
 
         commons = []
         for key in u1_ids:
             if key in u2_ids:
                 commons.append(key)
 
-        return u1_ids, u2_ids, commons
+        return u1_ids, u2_ids, commons, cached
 
-    def get_follower_matches(self, user1, user2):
-        u1_followed_by = self.api.GetFollowerIDs(screen_name=user1)
+    def get_follower_matches(self, user1, user2, allow_cached=True):
+        cached = False
 
-        u2_followed_by = self.api.GetFollowerIDs(screen_name=user2)
+        u1_followed_by = self.r.get(user1 + '.followed')
+        if u1_followed_by is None or allow_cached == False:
+            print 'redis missing ' + str(user1 + '.followed')
+            u1_followed_by = self.api.GetFollowerIDs(screen_name=user1)
+            self.r.set(user1 + '.' + 'followed', json.dumps(u1_followed_by))
+        else:
+            cached = True
+            u1_followed_by = json.loads(u1_followed_by)
+            print 'redis had ' + str(len(u1_followed_by)) + ' for ' + user1
+
+        u2_followed_by = self.r.get(user2 + '.followed')
+        if u2_followed_by is None or allow_cached == False:
+            print 'redis missing ' + str(user2 + '.followed')
+            u2_followed_by = self.api.GetFollowerIDs(screen_name=user2)
+            self.r.set(user2 + '.' + 'followed', json.dumps(u2_followed_by))
+        else:
+            cached = True
+            u2_followed_by = json.loads(u2_followed_by)
+            print 'redis had ' + str(len(u2_followed_by)) + ' for ' + user2
 
         commons_followed_by = []
         for key in u1_followed_by:
             if key in u2_followed_by:
                 commons_followed_by.append(key)
 
-        return u1_followed_by, u2_followed_by, commons_followed_by
+        return u1_followed_by, u2_followed_by, commons_followed_by, cached
 
--- overlaps/views.py
+++ overlaps/views.py
@@ -10,21 +10,32 @@ def home(request):
 
 
 def twitter_compare(request):
+    print request.GET
     twitter_name_1 = request.GET.get('twitter_name_one', '')
     twitter_name_2 = request.GET.get('twitter_name_two', '')
 
+    allow_cached = True
+    user_allowing_cache = request.GET.get('allow_cache', '')
+    print user_allowing_cache
+    if user_allowing_cache is not '':
+        allow_cached = False
+
     compare = Compare()
     try:
-        u1_ids, u2_ids, commons = compare.get_matches(twitter_name_1, twitter_name_2)
-        u1_followed_by, u2_followed_by, commons_followed_by = compare.get_follower_matches(twitter_name_1, twitter_name_2)
+        u1_ids, u2_ids, commons, cached = compare.get_matches(twitter_name_1, twitter_name_2, allow_cached)
+        u1_followed_by, u2_followed_by, commons_followed_by, cached_followed_by = compare.get_follower_matches(twitter_name_1, twitter_name_2, allow_cached)
 
         full_text = 'Commonalities between {} and {} : {} / ({}) / {}'.format(twitter_name_1, twitter_name_2, len(u1_ids), len(commons), len(u2_ids))
-        text_1 = '{} : {}'.format(twitter_name_1, float(len(commons))/len(u1_ids))
-        text_2 = '{} : {}'.format(twitter_name_2, float(len(commons))/len(u2_ids))
+        # text_1 = '{} : {}'.format(twitter_name_1, float(len(commons))/len(u1_ids))
+        # text_2 = '{} : {}'.format(twitter_name_2, float(len(commons))/len(u2_ids))
 
         followed_by_full = 'Commonalities between {} and {} : {} / ({}) / {}'.format(twitter_name_1, twitter_name_2, len(u1_followed_by), len(commons_followed_by), len(u2_followed_by))
-        followed_text_1 = '{} : {}'.format(twitter_name_1, float(len(commons_followed_by))/len(u1_followed_by))
-        followed_text_2 = '{} : {}'.format(twitter_name_2, float(len(commons_followed_by))/len(u2_followed_by))
+        # followed_text_1 = '{} : {}'.format(twitter_name_1, float(len(commons_followed_by))/len(u1_followed_by))
+        # followed_text_2 = '{} : {}'.format(twitter_name_2, float(len(commons_followed_by))/len(u2_followed_by))
+
+        cache_used = False
+        if cached or cached_followed_by:
+            cache_used = True
 
         return render_to_response('twitter_compare.html', {
             'twitter_name_one': twitter_name_1,
@@ -45,6 +56,7 @@ def twitter_compare(request):
             'followed_commons_size': len(commons_followed_by),
             't1_followed_percent': '{0:.2f}'.format(float(len(commons_followed_by))/len(u1_followed_by) * 100),
             't2_followed_percent': '{0:.2f}'.format(float(len(commons_followed_by))/len(u2_followed_by) * 100),
+            'cached': cache_used
         })
 
     except TwitterError as t:
